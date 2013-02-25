1a0d2d9e0f1102b79f8eb48f14d390b7108e7db6
Marek Mottl <kulhajss@gmail.com>
2013-02-25 17:00:11 +0100

    FlashDoor.cs added, not functional yet

--- Test/Test/EnemyHandler.cs
+++ Test/Test/EnemyHandler.cs
@@ -8,24 +8,35 @@ namespace Test
     class EnemyHandler
     {
         List<Tower> towers;
+        List<FlashDoor> doors;
 
         public EnemyHandler()
         {
             towers = new List<Tower>();
+            doors = new List<FlashDoor>();
         }
 
         public void Initiliaze()
         {
+
+            //_________________________TOWERS________________________________
             towers.Add(new Tower(new Vector2(1024, 256 + 24 * 1 / 0.667f)));
             towers.Add(new Tower(new Vector2(1536, 64 + 24 * 1 / 0.667f)));
             towers.Add(new Tower(new Vector2(1664, 256 + 24 * 1 / 0.667f)));
             towers.Add(new Tower(new Vector2(2176, 128 + 24 * 1 / 0.667f)));
+            //_________________________DOORS_________________________________
+            doors.Add(new FlashDoor(new Vector2(1088, 256 - 48), new Vector2(100, 100)));
         }
 
         public void LoadContent(ContentManager theContentManager)
         {
             foreach (Tower t in towers)
-                t.LoadContent(theContentManager);
+
+                if (t.GetType() == typeof(Tower))
+                    t.LoadContent(theContentManager);
+
+            foreach (FlashDoor f in doors)
+                f.LoadContent(theContentManager);
         }
 
         public void Update(Player player, GameTime theGameTime)
@@ -38,6 +49,9 @@ namespace Test
         {
             foreach (Tower t in towers)
                 t.Draw(theSpriteBatch);
+
+            foreach (FlashDoor f in doors)
+                f.Draw(theSpriteBatch);
         }
     }
 }
---
+++ Test/Test/FlashDoor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Test
+{
+    class FlashDoor : Sprite
+    {
+        Texture2D _switch;
+        Texture2D door;
+
+        int currentFrame = 0;
+        int animationLenght = 16;
+
+        Vector2 switchPosition;
+
+        Rectangle[] sources = new Rectangle[] {
+            new Rectangle(0,0,64,192),
+            new Rectangle(64,0,64,192),
+            new Rectangle(0,192,64,64),
+            new Rectangle(64,192,64,64)
+        };
+
+        public bool Switch { get; set; }
+
+        public FlashDoor(Vector2 position, Vector2 switchPosition)
+        {
+            this.Position = position;
+            this.switchPosition = switchPosition;
+            this.Switch = true;
+        }
+
+        public void Update(GameTime theGameTime)
+        {
+            this.Animate();
+        }
+
+        private void Animate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadContent(ContentManager theContentManager)
+        {
+            contentManager = theContentManager;
+            _switch = contentManager.Load<Texture2D>("flashDoor");
+            door = contentManager.Load<Texture2D>("flashDoor");
+            Source = sources[0];
+        }
+        
+        public void Draw(SpriteBatch theSpriteBatch)
+        {
+            theSpriteBatch.Draw(_switch, switchPosition, Switch ? sources[2] : sources[3], Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 0.0f);
+            theSpriteBatch.Draw(door, Position, Source, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 0.0f);
+        }
+    }
+}
